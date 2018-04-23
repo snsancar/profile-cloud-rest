e70272817499650642a222f3d7268bcdd1daea39
snsancar <sancar.sn@gmail.com>
2018-04-23 07:21:46 +0530

    modified:   src/test/java/com/apsis/profile/ProfileCloudApplicationTests.java

--- src/test/java/com/apsis/profile/ProfileCloudApplicationTests.java
+++ src/test/java/com/apsis/profile/ProfileCloudApplicationTests.java
@@ -1,16 +1,29 @@
 package com.apsis.profile;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ProfileCloudApplicationTests {
+	
+	@Autowired
+    private MockMvc mockMvc;
 
 	@Test
-	public void contextLoads() {
-	}
+    public void testListAllCounters() throws Exception {
+
+        this.mockMvc.perform(get("/profile/counters")).andDo(print()).andExpect(status().isOk());
+    }
 
 }
